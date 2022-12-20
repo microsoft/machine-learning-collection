66cc4620b055ca557548163e6097d52bc63f37be
Keita Onabuta <keonabut@microsoft.com>
2022-12-21 00:07:43 +0900

    minor change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,5 @@ module.exports = {
   rules: {
     // Note: you must disable the base rule as it can report incorrect errors
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
